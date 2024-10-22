408acbbf7ce67253b65a474db6f823adbaacb727
ashwinnagelwar <ashwinnagelwar@gmail.com>
2024-10-22 15:27:06 +0530

    retyr mechanism is addedd

--- User/src/main/java/com/user/config/UserConfig.java
+++ User/src/main/java/com/user/config/UserConfig.java
@@ -1,5 +1,6 @@
 package com.user.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class UserConfig {
 
 	@Bean
-	public RestTemplate getRestTemplate()
-	{
-		return new RestTemplate();
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	    return builder.build();
 	}
 }
--- User/src/main/java/com/user/service/UserService.java
+++ User/src/main/java/com/user/service/UserService.java
@@ -8,6 +8,7 @@ import com.user.entity.Address;
 import com.user.entity.User;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Service
 public class UserService {
@@ -16,7 +17,7 @@ public class UserService {
 	    private RestTemplate restTemplate;
 
 	    @CircuitBreaker(name = "userService", fallbackMethod = "fallbackGetUserDetails")
-	    
+	    @Retry(name = "userService", fallbackMethod = "fallbackGetUserDetails")
 	    public User getUserDetails() {
 	        Address address = restTemplate.getForObject("http://localhost:8081/address/getAddress", Address.class);
 	        User user = new User();
@@ -28,9 +29,23 @@ public class UserService {
 	        return user;
 	    }
 
-	    // Fallback method
+	    // Fallback method for circuit breaker
+//	    public User fallbackGetUserDetails(Throwable t) {
+//	        System.out.println("Address service is down. Fallback executed.");
+//	        User user = new User();
+//	        user.setId(1);
+//	        user.setFName("dummy");
+//	        user.setLName("dummy");
+//	        user.setAge(32);
+//	        user.setAddress(null);
+//	        return user;
+//	    }
+	    
+	    
+	    //fallback method for Retry
+	    int retryCount=1;
 	    public User fallbackGetUserDetails(Throwable t) {
-	        System.out.println("Address service is down. Fallback executed.");
+	        System.out.println("Address service is down. Fallback executed."+retryCount);
 	        User user = new User();
 	        user.setId(1);
 	        user.setFName("dummy");
